052869c82c92aba7483f31ccb23f2f38489d91b7
Dave Whipps <dwhipps@me.com>
2023-12-10 11:48:01 -0500

    Cleaned up single modality training code.

--- code/train.py
+++ code/train.py
@@ -8,6 +8,8 @@ import numpy as np
 from sklearn.utils import class_weight 
 from collections import Counter
 import platform
+from utilities import plot_history, read_hyperparameters, get_class_weights, get_base_pipeline
+
 
 #Parametrize hyperparams so we can grid search
 HYPERPARAMS = {
@@ -17,7 +19,7 @@ HYPERPARAMS = {
   'IMG_SIZE' : (512, 384),
   'REGULARIZATION' : 0.5,
   'BASE_LEARNING_RATE' : 0.0001,
-  'USE_CACHING' : False,
+  'USE_CACHING' : False
 }
 
 # Main entry point
@@ -31,87 +33,61 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	# Store the output directory
-	OUTPUT_DIR = args.output_dir
+	outpur_dir = args.output_dir
 
 	# Read in hyperparams from YAML file, if any
-	params_file = None
 	if args.params_file:
-		with open(args.params_file, 'r') as stream:
-			try:
-				params_file = yaml.load(stream, Loader=yaml.FullLoader)
-				# Override any hyperparams with those in the YAML file
-				for key, value in params_file.items():
-					HYPERPARAMS[key] = value  
-				print(HYPERPARAMS)
+		HYPERPARAMS = read_hyperparameters( args.params_file, HYPERPARAMS )
 
-			except yaml.YAMLError as exc:
-				print(exc)
+	# rename for readability
+	batch_size = HYPERPARAMS['BATCH_SIZE']
+	image_size = HYPERPARAMS['IMG_SIZE']
+	regularization = HYPERPARAMS['REGULARIZATION']
+	learning_rate = HYPERPARAMS['BASE_LEARNING_RATE']
+	patience = HYPERPARAMS['PATIENCE']
+	epochs = HYPERPARAMS['NUM_EPOCHS']
+	modality = HYPERPARAMS['MODALITY']
 
 	# Read in the training data
 	train_dir = os.path.join(args.data_dir, 'train')
 	train_data = tf.keras.utils.image_dataset_from_directory(train_dir,
 															seed=42,
 															shuffle=True,
-															batch_size=HYPERPARAMS['BATCH_SIZE'],
-															image_size=HYPERPARAMS['IMG_SIZE'])
+															batch_size=batch_size,
+															image_size=image_size)
 
 	# Read in the validation data
 	validation_dir = os.path.join(args.data_dir, 'val')
 	val_data = tf.keras.utils.image_dataset_from_directory(validation_dir,
 															seed=42,
 															shuffle=True,
-															batch_size=HYPERPARAMS['BATCH_SIZE'],
-															image_size=HYPERPARAMS['IMG_SIZE'])
-
-	# Tease out the class names
-	CLASS_NAMES = train_data.class_names
-	NUM_CLASSES = len(CLASS_NAMES)
-	print(CLASS_NAMES, " Num Classes: ", NUM_CLASSES) # Output the class names
+															batch_size=batch_size,
+															image_size=image_size)
 
 	# Prefetch for performance
-	AUTOTUNE = tf.data.AUTOTUNE
-	train_data = train_data.prefetch(buffer_size=AUTOTUNE)
-	val_data = val_data.prefetch(buffer_size=AUTOTUNE)
+	train_data = train_data.prefetch(buffer_size=tf.data.AUTOTUNE)
+	val_data = val_data.prefetch(buffer_size=tf.data.AUTOTUNE)
 
-	# Our dataset is imbalanced, so calculate the class weights
-	train_labels = np.concatenate([y for x, y in train_data], axis=0)
-	class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(train_labels), y=train_labels)
-	class_weights = dict(enumerate(class_weights)) 
+	# The L-AVATeD dataset has unbalanced classes, and so we need to account for that 
+	# by weighting our loss
+	class_weights, num_classes = get_class_weights( train_dir )
 	print("Class Weights: ", class_weights) # Output the class weights
 
 	# Create the base model from the pre-trained model MobileNet V2
-	IMG_SHAPE = HYPERPARAMS['IMG_SIZE'] + (3,)
-	print("Image Shape ", IMG_SHAPE)
-	base_model = tf.keras.applications.MobileNetV2(	input_shape=IMG_SHAPE,
-													include_top=False,
-													weights='imagenet')
-
-	# We want to parametrize whether we want to train the entire model so we can test
-	base_model.trainable = HYPERPARAMS['WANTS_TRAIN_FULL_MODEL']
-
-	# Define pipeline
-	inputs = tf.keras.Input(shape=IMG_SHAPE)
-	# Our dataset is somewhat limited, so do some data augmentation to beef it up
-	x = tf.keras.layers.RandomRotation(0.05)(inputs) 
-	# We're using MobileNetV2, so use the built-in preprocessing for that base model
-	x = tf.keras.applications.mobilenet_v2.preprocess_input(x) #rescales from -1..1 for MobileNetV2
-	# Call MobileNetV2
-	x = base_model(x, training=False)
-	x = tf.keras.layers.GlobalAveragePooling2D()(x)
-	x = tf.keras.layers.Dropout(HYPERPARAMS['REGULARIZATION'])(x) # regularize for better generalization
-	outputs = tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')(x)
+	image_shape = image_size + (3,)
+	print("Image Shape ", image_shape)
+	( inputs, pipeline ) = get_base_pipeline( image_shape, batch_size, regularization, modality+'_')
+	outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(pipeline)
 	model = tf.keras.Model(inputs, outputs)
 
 	# print a summary of our full model
 	model.summary()
 
-	#Compile the model
-	# WARNING:absl:At this time, the v2.11+ optimizer `tf.keras.optimizers.Adam` runs slowly on M1/M2 Macs
-	# please use the legacy Keras optimizer instead, located at `tf.keras.optimizers.legacy.Adam`.
+	# If you're running on an ARM (aka Apple Silicon) processor, this will speed up training
 	if platform.processor() == 'arm':
-		optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=HYPERPARAMS['BASE_LEARNING_RATE'])
+		optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate)
 	else:
-		optimizer = tf.keras.optimizers.Adam(learning_rate=HYPERPARAMS['BASE_LEARNING_RATE'])
+		optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
 	model.compile(	optimizer=optimizer,
 					loss=tf.keras.losses.SparseCategoricalCrossentropy(),
@@ -119,53 +95,26 @@ if __name__ == "__main__":
 
 	# Use checkpointing so that we can resume, as well as test and export
 	date_time_string = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-	checkpoint_path = os.path.join(OUTPUT_DIR, "checkpoints", date_time_string, "cp-{epoch:04d}.ckpt")
+	checkpoint_path = os.path.join(outpur_dir, "checkpoints", date_time_string, "cp-{epoch:04d}.ckpt")
 	checkpoint_dir = os.path.dirname(checkpoint_path)
 	checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_dir,
 							  								 save_best_only=True,
                                                              verbose=1)
   	
 	# Use early stopping so we don't need to worry too much about our NUM_EPOCHS
-	early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=HYPERPARAMS['PATIENCE'])
+	early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
 
 	# Train
 	history = model.fit(train_data,
 						validation_data=val_data,
-						epochs=HYPERPARAMS['NUM_EPOCHS'],
+						epochs=epochs,
 						class_weight=class_weights,
 						callbacks=[checkpoint_callback, early_stopping])
 
 	# Save out the "best" model
-	final_model_path = os.path.join(OUTPUT_DIR, "saved_models", date_time_string)
+	final_model_path = os.path.join(outpur_dir, "saved_models", date_time_string)
 	os.makedirs(final_model_path, exist_ok = True)
 	model.save(final_model_path+'/s2mnet_model')
 
 	# Plot learning curves, and save as an image
-	acc = history.history['accuracy']
-	val_acc = history.history['val_accuracy']
-	loss = history.history['loss']
-	val_loss = history.history['val_loss']
-
-	plt.figure(figsize=(8, 8))
-	plt.subplot(2, 1, 1)
-	plt.plot(acc, label='Training Accuracy')
-	plt.plot(val_acc, label='Validation Accuracy')
-	plt.legend(loc='upper right')
-	plt.ylabel('Accuracy')
-	plt.ylim([0,1.0])
-	plt.title('Training and Validation Accuracy')
-
-	plt.subplot(2, 1, 2)
-	plt.plot(loss, label='Training Loss')
-	plt.plot(val_loss, label='Validation Loss')
-	plt.legend(loc='upper right')
-	plt.ylabel('Cross Entropy')
-	plt.ylim([0,3.0])
-	plt.title('Training and Validation Loss')
-	plt.xlabel('epoch')
-
-	learning_curves_file = os.path.join(OUTPUT_DIR, "plots", date_time_string, "learning_curves.png")
-	training_plots_dir = os.path.dirname(learning_curves_file)
-	if not os.path.isdir(training_plots_dir):
-		os.makedirs(training_plots_dir)
-	plt.savefig(learning_curves_file, bbox_inches='tight')
+	plot_history( history, outpur_dir, date_time_string )
