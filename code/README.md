aa5119aac2a9ceece861885787ca300793ef0c22
Dave Whipps <dwhipps@me.com>
2023-12-10 14:21:50 -0500

    Updated ReadMe. Auto formatted code.

--- code/README.md
+++ code/README.md
@@ -1,11 +1,34 @@
-# S2M Image Classifier Training and Testing
+# Image Classifier Training and Testing
 
 ## Overview
+This folder contains python code to train a CNN image classifier on the L-AVATeD Image Dataset. 
 
-This folder contains python code to train a CNN image classifier on the S2Mnet Image Dataset, the location of which must be pre-separated into train, validation and test subfolders (easily accomplished using `s2mnet_data_prep/copy_and_split_data.py`).
+(NOTE: The L-AVATeD dataset must be present on your local machine. The default/expected location is in the `../data/` folder.)
 
-Model training is initiated with the `train_classifer.sh` script. Several hyperparameters can be modified using the hyperparameters.yaml file.
+Simple RGB or LiDAR training is initiated with `train.py` passing appropriate command line arguments. Several hyperparameters can be modified using the `hyperparameters/hyperparams_[rgb,lidar].yaml` files.
 
-Model performance can be checked using the script `test_classifier.sh` which calls `test.py` with several hyperparameters.
+For example, to train a simple classifier on the RGB data:
+```
+> python train.py --data_dir "../data/lavated_rgb_split" --params_file "hyperparameters/hyperparams_rgb.yaml" --output_dir "../output"  
+```
+
+Simple RGB or LiDAR Model performance can be checked using the script `test.py` passing appropriate command line arguments.
+
+For example, to test a model pre-trained on the LiDAR data:
+```
+> python test.py --model_dir "../models/lavated-lidar" --data_dir "../data/lavated_lidar_split" --output_dir "../output" 
+```
+
+Multimodal RGB-D (RGB and LiDAR) image pair model training is initiated with `train_rgbd.py`. Several hyperparameters can be modified using the `hyperparameters/hyperparams_rgbd.yaml` file.
+
+```
+> python train_rgbd.py --data_dir "../data" --params_file "hyperparameters/hyperparams_rgbd.yaml" --output_dir "../output"  
+```
+
+Simple RGB-D Model performance can be checked using the script `test_rgbd.py` passing appropriate command line arguments.
+
+```
+> python test_rgbd.py --model_dir "../models/lavated-rgbd" --data_dir "../data" --output_dir "../output" 
+```
 
 By default, model checkpoints, learning curve plots, and best models are written to an `output` folder with a date stamp.
--- code/train.py
+++ code/train.py
@@ -5,116 +5,122 @@ import yaml
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.utils import class_weight 
+from sklearn.utils import class_weight
 from collections import Counter
 import platform
 from utilities import plot_history, read_hyperparameters, get_class_weights, get_base_pipeline
 
 
-#Parametrize hyperparams so we can grid search
+# Parametrize hyperparams so we can grid search
 HYPERPARAMS = {
-  'WANTS_TRAIN_FULL_MODEL'  : True,
-  'NUM_EPOCHS' : 20,
-  'BATCH_SIZE' : 32,
-  'IMG_SIZE' : (512, 384),
-  'REGULARIZATION' : 0.5,
-  'BASE_LEARNING_RATE' : 0.0001,
-  'USE_CACHING' : False
+    'WANTS_TRAIN_FULL_MODEL': True,
+    'NUM_EPOCHS': 20,
+    'BATCH_SIZE': 32,
+    'IMG_SIZE': (512, 384),
+    'REGULARIZATION': 0.5,
+    'BASE_LEARNING_RATE': 0.0001,
+    'USE_CACHING': False
 }
 
 # Main entry point
 if __name__ == "__main__":
-	# Get the command line args
+    # Get the command line args
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--data_dir', type=str, required=True )
-	parser.add_argument('--output_dir', type=str, required=True )
-	parser.add_argument('--params_file', type=str, required=False )
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, required=True)
+    parser.add_argument('--output_dir', type=str, required=True)
+    parser.add_argument('--params_file', type=str, required=False)
+    args = parser.parse_args()
 
-	# Store the output directory
-	outpur_dir = args.output_dir
+    # Store the output directory
+    outpur_dir = args.output_dir
 
-	# Read in hyperparams from YAML file, if any
-	if args.params_file:
-		HYPERPARAMS = read_hyperparameters( args.params_file, HYPERPARAMS )
+    # Read in hyperparams from YAML file, if any
+    if args.params_file:
+        HYPERPARAMS = read_hyperparameters(args.params_file, HYPERPARAMS)
 
-	# rename for readability
-	batch_size = HYPERPARAMS['BATCH_SIZE']
-	image_size = HYPERPARAMS['IMG_SIZE']
-	regularization = HYPERPARAMS['REGULARIZATION']
-	learning_rate = HYPERPARAMS['BASE_LEARNING_RATE']
-	patience = HYPERPARAMS['PATIENCE']
-	epochs = HYPERPARAMS['NUM_EPOCHS']
-	modality = HYPERPARAMS['MODALITY']
+    # rename for readability
+    batch_size = HYPERPARAMS['BATCH_SIZE']
+    image_size = HYPERPARAMS['IMG_SIZE']
+    regularization = HYPERPARAMS['REGULARIZATION']
+    learning_rate = HYPERPARAMS['BASE_LEARNING_RATE']
+    patience = HYPERPARAMS['PATIENCE']
+    epochs = HYPERPARAMS['NUM_EPOCHS']
+    modality = HYPERPARAMS['MODALITY']
 
-	# Read in the training data
-	train_dir = os.path.join(args.data_dir, 'train')
-	train_data = tf.keras.utils.image_dataset_from_directory(train_dir,
-															seed=42,
-															shuffle=True,
-															batch_size=batch_size,
-															image_size=image_size)
+    # Read in the training data
+    train_dir = os.path.join(args.data_dir, 'train')
+    train_data = tf.keras.utils.image_dataset_from_directory(train_dir,
+                                                             seed=42,
+                                                             shuffle=True,
+                                                             batch_size=batch_size,
+                                                             image_size=image_size)
 
-	# Read in the validation data
-	validation_dir = os.path.join(args.data_dir, 'val')
-	val_data = tf.keras.utils.image_dataset_from_directory(validation_dir,
-															seed=42,
-															shuffle=True,
-															batch_size=batch_size,
-															image_size=image_size)
+    # Read in the validation data
+    validation_dir = os.path.join(args.data_dir, 'val')
+    val_data = tf.keras.utils.image_dataset_from_directory(validation_dir,
+                                                           seed=42,
+                                                           shuffle=True,
+                                                           batch_size=batch_size,
+                                                           image_size=image_size)
 
-	# Prefetch for performance
-	train_data = train_data.prefetch(buffer_size=tf.data.AUTOTUNE)
-	val_data = val_data.prefetch(buffer_size=tf.data.AUTOTUNE)
+    # Prefetch for performance
+    train_data = train_data.prefetch(buffer_size=tf.data.AUTOTUNE)
+    val_data = val_data.prefetch(buffer_size=tf.data.AUTOTUNE)
 
-	# The L-AVATeD dataset has unbalanced classes, and so we need to account for that 
-	# by weighting our loss
-	class_weights, num_classes = get_class_weights( train_dir )
-	print("Class Weights: ", class_weights) # Output the class weights
+    # The L-AVATeD dataset has unbalanced classes, and so we need to account for that
+    # by weighting our loss
+    class_weights, num_classes = get_class_weights(train_dir)
+    print("Class Weights: ", class_weights)  # Output the class weights
 
-	# Create the base model from the pre-trained model MobileNet V2
-	image_shape = image_size + (3,)
-	print("Image Shape ", image_shape)
-	( inputs, pipeline ) = get_base_pipeline( image_shape, batch_size, regularization, modality+'_')
-	outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(pipeline)
-	model = tf.keras.Model(inputs, outputs)
+    # Create the base model from the pre-trained model MobileNet V2
+    image_shape = image_size + (3,)
+    print("Image Shape ", image_shape)
+    (inputs, pipeline) = get_base_pipeline(
+        image_shape, batch_size, regularization, modality+'_')
+    outputs = tf.keras.layers.Dense(
+        num_classes, activation='softmax')(pipeline)
+    model = tf.keras.Model(inputs, outputs)
 
-	# print a summary of our full model
-	model.summary()
+    # print a summary of our full model
+    model.summary()
 
-	# If you're running on an ARM (aka Apple Silicon) processor, this will speed up training
-	if platform.processor() == 'arm':
-		optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate)
-	else:
-		optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    # If you're running on an ARM (aka Apple Silicon) processor, this will speed up training
+    if platform.processor() == 'arm':
+        optimizer = tf.keras.optimizers.legacy.Adam(
+            learning_rate=learning_rate)
+    else:
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
-	model.compile(	optimizer=optimizer,
-					loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-					weighted_metrics = ['accuracy'] )
+    model.compile(optimizer=optimizer,
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  weighted_metrics=['accuracy'])
 
-	# Use checkpointing so that we can resume, as well as test and export
-	date_time_string = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-	checkpoint_path = os.path.join(outpur_dir, "checkpoints", date_time_string, "cp-{epoch:04d}.ckpt")
-	checkpoint_dir = os.path.dirname(checkpoint_path)
-	checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_dir,
-							  								 save_best_only=True,
+    # Use checkpointing so that we can resume, as well as test and export
+    date_time_string = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    checkpoint_path = os.path.join(
+        outpur_dir, "checkpoints", date_time_string, "cp-{epoch:04d}.ckpt")
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_dir,
+                                                             save_best_only=True,
                                                              verbose=1)
-  	
-	# Use early stopping so we don't need to worry too much about our NUM_EPOCHS
-	early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
 
-	# Train
-	history = model.fit(train_data,
-						validation_data=val_data,
-						epochs=epochs,
-						class_weight=class_weights,
-						callbacks=[checkpoint_callback, early_stopping])
+    # Use early stopping so we don't need to worry too much about our NUM_EPOCHS
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss', patience=patience)
 
-	# Save out the "best" model
-	final_model_path = os.path.join(outpur_dir, "saved_models", date_time_string)
-	os.makedirs(final_model_path, exist_ok = True)
-	model.save(final_model_path+'/s2mnet_model')
+    # Train
+    history = model.fit(train_data,
+                        validation_data=val_data,
+                        epochs=epochs,
+                        class_weight=class_weights,
+                        callbacks=[checkpoint_callback, early_stopping])
 
-	# Plot learning curves, and save as an image
-	plot_history( history, outpur_dir, date_time_string )
+    # Save out the "best" model
+    final_model_path = os.path.join(
+        outpur_dir, "saved_models", date_time_string)
+    os.makedirs(final_model_path, exist_ok=True)
+    model.save(final_model_path+'/s2mnet_model')
+
+    # Plot learning curves, and save as an image
+    plot_history(history, outpur_dir, date_time_string)
